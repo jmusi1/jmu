c06fb1aaa2c1769c22b87a76ca1410efb47cc956
jmusi1 <rheisenberg04@gmail.com>
2024-09-09 16:15:58 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,54 +1,54 @@
-package com.example.secondprogramm;
+package com.example.signupandloginin;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    EditText e1, e2;
-    TextView tv1;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+EditText Signup_Username, Signup_Password;
+Button btnSignup;
+String regularExpression="^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!])[A-Za-z\\d@$!]{8,}$";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        e1 =  findViewById(R.id.editText1);
-        e2 = (EditText) findViewById(R.id.editText2);
-        tv1 = (TextView) findViewById(R.id.textView1);
+        Signup_Username=(EditText)findViewById(R.id.txt_username);
+        Signup_Password=(EditText)findViewById(R.id.txt_password);
+        btnSignup=(Button)findViewById(R.id.btn_signup);
+        btnSignup.setOnClickListener(this);
     }
 
-    public void doAdd(View V) {
-        int a1 = Integer.parseInt(e1.getText().toString());
-        int a2 = Integer.parseInt(e2.getText().toString());
-        int result = a1 + a2;
-        tv1.setText("" + result);
-    }
-
-    public void doSub(View V) {
-        int a1 = Integer.parseInt(e1.getText().toString());
-        int a2 = Integer.parseInt(e2.getText().toString());
-        int result = a1 - a2;
-        tv1.setText("" + result);
-    }
-
-    public void doMul(View V) {
-        int a1 = Integer.parseInt(e1.getText().toString());
-        int a2 = Integer.parseInt(e2.getText().toString());
-        int result = a1 * a2;
-        tv1.setText("" + result);
-    }
-
-    public void doDiv(View V) {
-        int a1 = Integer.parseInt(e1.getText().toString());
-        int a2 = Integer.parseInt(e2.getText().toString());
-        if (a2 != 0) {
-            float result = (float) a1 / a2;
-            tv1.setText("" + result);
-        } else {
-            tv1.setText("Cannot divide by zero");
+    @Override
+    public void onClick(View v) {
+        String username=Signup_Username.getText().toString();
+        String password=Signup_Password.getText().toString();
+        if(validatePassword(password)){
+            Toast.makeText(getBaseContext(),"Valid Password", Toast.LENGTH_LONG).show();
+            Bundle bundle=new Bundle();
+            bundle.putString("User",username);
+            bundle.putString("Pwd",password);
+            Intent it=new Intent(this,Login.class);
+            it.putExtra("data",bundle);
+            startActivity(it);
+        } else
+        {
+            Toast.makeText(getBaseContext(),"Invalid Password",Toast.LENGTH_LONG).show();
         }
+
+    }
+
+    private boolean validatePassword(String password) {
+        Pattern pattern=Pattern.compile(regularExpression);
+        Matcher matcher=pattern.matcher(password);
+        return matcher.matches();
     }
 }
